45435f84f1d032d5c55320acbdf66a3b04ae146f
adachkamil98 <adachkamil98@gmail.com>
2023-02-15 23:56:00 +0100

    adding empty task prevention code added

--- src/Form/index.js
+++ src/Form/index.js
@@ -6,7 +6,13 @@ const Form = ({ addNewTask }) => {
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    addNewTask(newTaskContent.trim());
+    if (newTaskContent.trim() === "") {
+      setNewTaskContent("")
+      return;
+
+    } else {
+      addNewTask(newTaskContent.trim())
+    };
     setNewTaskContent("");
   }
 
