04804848c9a6f8f7f66c731f4735ea48a61bc516
adachkamil98 <adachkamil98@gmail.com>
2023-04-03 21:01:39 +0200

    common, features folders created

---
+++ src/features/tasks/Tasks.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import Form from "./Form";
+import Buttons from "./Buttons";
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import Container from "../../common/Container";
+import { useTasks } from "../../useTasks";
+import TasksList from "./TasksList";
+
+function Tasks() {
+  const [hideDone, setHideDone] = useState(false);
+
+  const toggleHideDone = () => {
+    setHideDone((hideDone) => !hideDone);
+  };
+
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllTasksAsDone,
+    addNewTask,
+  } = useTasks();
+
+  return (
+    <Container>
+      <Header title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
+      <Section
+        title="Lista zadań"
+        body={
+          <TasksList
+            tasks={tasks}
+            hideDone={hideDone}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
+        extraHeaderContent={
+          <Buttons
+            tasks={tasks}
+            hideDone={hideDone}
+            toggleHideDone={toggleHideDone}
+            setAllTasksAsDone={setAllTasksAsDone}
+          />
+        }
+      />
+    </Container>
+  );
+}
+
+export default Tasks;
